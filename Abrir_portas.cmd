b2645d8b58dead3e4984050db9dab7b3aaaa0f04
pll1409 <apollo140921@gmail.com>
2025-07-19 21:01:09 -0300

    Add files via upload

---
+++ Abrir_portas.cmd
@@ -0,0 +1,40 @@
+netsh advfirewall firewall add rule name="30120 TCP" dir=in action=allow protocol=TCP localport=30120
+cls
+netsh advfirewall firewall add rule name="30120 UDP" dir=in action=allow protocol=UDP localport=30120
+cls
+netsh advfirewall firewall add rule name="30120 TCP" dir=out action=allow protocol=TCP localport=30120
+cls
+netsh advfirewall firewall add rule name="30120 UDP" dir=out action=allow protocol=UDP localport=30120
+cls
+netsh advfirewall firewall add rule name="3306 TCP" dir=in action=allow protocol=TCP localport=3306
+cls
+netsh advfirewall firewall add rule name="3306 UDP" dir=in action=allow protocol=UDP localport=3306
+cls
+netsh advfirewall firewall add rule name="3306 TCP" dir=out action=allow protocol=TCP localport=3306
+cls
+netsh advfirewall firewall add rule name="3306 UDP" dir=out action=allow protocol=UDP localport=3306
+cls
+netsh advfirewall firewall add rule name="30120 TCP" dir=in action=allow protocol=TCP localport=30120
+cls
+netsh advfirewall firewall add rule name="30120 UDP" dir=in action=allow protocol=UDP localport=30120
+cls
+netsh advfirewall firewall add rule name="30120 TCP" dir=out action=allow protocol=TCP localport=30120
+cls
+netsh advfirewall firewall add rule name="30120 UDP" dir=out action=allow protocol=UDP localport=30120
+cls
+netsh advfirewall firewall add rule name="3000 TCP" dir=in action=allow protocol=TCP localport=3000
+cls
+netsh advfirewall firewall add rule name="3000 UDP" dir=in action=allow protocol=UDP localport=3000
+cls
+netsh advfirewall firewall add rule name="3000 TCP" dir=out action=allow protocol=TCP localport=3000
+cls
+netsh advfirewall firewall add rule name="3000 UDP" dir=out action=allow protocol=UDP localport=3000
+cls
+netsh advfirewall firewall add rule name="80 TCP" dir=in action=allow protocol=TCP localport=80
+cls
+netsh advfirewall firewall add rule name="80 UDP" dir=in action=allow protocol=UDP localport=80
+cls
+netsh advfirewall firewall add rule name="80 TCP" dir=out action=allow protocol=TCP localport=80
+cls
+netsh advfirewall firewall add rule name="80 UDP" dir=out action=allow protocol=UDP localport=80
+cls
---
+++ start.bat
@@ -0,0 +1,3 @@
+
+start artifacts/FXServer.exe +exec server.cfg +set onesync_enableInfinity 1 +set sv_enforceGameBuild 2372
+exit
